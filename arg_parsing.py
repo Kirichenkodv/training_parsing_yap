eb764f869beac5314c5f6ddcfa5dc8f0c0b6da5d
kirichenko dmitriy <kirichenko.dmitriy1977@gmail.com>
2025-01-25 15:28:00 +1000

    add name, surname

--- arg_parsing.py
+++ arg_parsing.py
@@ -1,10 +1,15 @@
-import sys
+import argparse
 
 if __name__ == '__main__':
-    # Печать всех аргументов.
-    print('All arguments:', sys.argv)
-    # Проверка того, чтобы при запуске программы после 
-    # названия скрипта было введено имя.
-    if len(sys.argv) == 2:
-        # Печать первого элемента после названия скрипта.
-        print('Hello,', sys.argv[1])
+    parser = argparse.ArgumentParser(description='Вежливый скрипт')
+    parser.add_argument('name', help='Имя') 
+    parser.add_argument('-s', '--surname', help='Фамилия')    
+    args = parser.parse_args()
+    parts = [] 
+    # Добавляем приветствие по имени.
+    parts.append(f'Hello, {args.name}')
+    # Если указана фамилия, то она тоже добавляется к выводу.
+    if args.surname is not None:
+        parts.append(args.surname)
+    # Печатаем через пробел все элементы списка parts.
+    print(*parts)
