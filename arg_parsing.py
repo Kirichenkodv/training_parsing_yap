a9dfc82f94e3436622becd38d5fa9e44be1e6db1
kirichenko dmitriy <kirichenko.dmitriy1977@gmail.com>
2025-01-25 15:35:24 +1000

    add Murzik

--- arg_parsing.py
+++ arg_parsing.py
@@ -1,22 +1,32 @@
 import argparse
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Вежливый скрипт")
-    parser.add_argument("name", help="Имя")
-    parser.add_argument("-s", "--surname", help="Фамилия")
+MURZIK = '=^..^=______/'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Вежливый скрипт')
+    parser.add_argument('name', help='Имя') 
+    parser.add_argument('-s', '--surname', help='Фамилия') 
+    parser.add_argument(
+        '-c', 
+        '--city', 
+        help='Город',
+        choices=['Chekhov', 'Dublin', 'Minsk', 'Simbirsk'],
+    )    
     # Новый аргумент.
     parser.add_argument(
-        "-c",
-        "--city",
-        help="Город",
-        choices=["Chekhov", "Dublin", "Minsk", "Simbirsk"],
+        '-m',
+        '--murzik',
+        action='store_true',
+        help=f'Отправить кота Мурзика {MURZIK}'
     )
     args = parser.parse_args()
-    parts = []
-    parts.append(f"Hello, {args.name}")
+    parts = [] 
+    parts.append(f'Hello, {args.name}')
     if args.surname is not None:
         parts.append(args.surname)
-    # Новое условие для вывода города.
     if args.city is not None:
-        parts.append(f"from {args.city}")
+        parts.append(f'from {args.city}')
+    # Новое условие для вывода изображения кота Мурзика.    
+    if args.murzik:
+        parts.append(MURZIK)
     print(*parts)
