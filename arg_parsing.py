7a9594653e7aa6080f0e212ef13a074315130a3f
kirichenko dmitriy <kirichenko.dmitriy1977@gmail.com>
2025-01-25 15:30:30 +1000

    add arg_parsing city

--- arg_parsing.py
+++ arg_parsing.py
@@ -1,15 +1,22 @@
 import argparse
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Вежливый скрипт')
-    parser.add_argument('name', help='Имя') 
-    parser.add_argument('-s', '--surname', help='Фамилия')    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Вежливый скрипт")
+    parser.add_argument("name", help="Имя")
+    parser.add_argument("-s", "--surname", help="Фамилия")
+    # Новый аргумент.
+    parser.add_argument(
+        "-c",
+        "--city",
+        help="Город",
+        choices=["Chekhov", "Dublin", "Minsk", "Simbirsk"],
+    )
     args = parser.parse_args()
-    parts = [] 
-    # Добавляем приветствие по имени.
-    parts.append(f'Hello, {args.name}')
-    # Если указана фамилия, то она тоже добавляется к выводу.
+    parts = []
+    parts.append(f"Hello, {args.name}")
     if args.surname is not None:
         parts.append(args.surname)
-    # Печатаем через пробел все элементы списка parts.
+    # Новое условие для вывода города.
+    if args.city is not None:
+        parts.append(f"from {args.city}")
     print(*parts)
